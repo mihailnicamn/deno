cff82a4e762023f7b38515c6ba1997adafdac152
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-12-27 17:00:42 +0100

    Remove poweredBy middleware from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import { compress, logger, poweredBy, secureHeaders } from 'hono/middlewares'
+import { compress, logger, secureHeaders } from 'hono/middlewares'
 import api from './api/index.ts'
 import { customLogger } from './logger.ts'
 import middlewares from './middlewares.ts'
@@ -11,7 +11,6 @@ if (Deno.env.get('ENV') !== 'test') {
   app.use('*', logger(customLogger))
 }
 app.use('*', secureHeaders())
-app.use('*', poweredBy())
 
 app.get('/', (c) =>
   c.json({
